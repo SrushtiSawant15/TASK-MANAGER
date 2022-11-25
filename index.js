cdd8325e5fd2e28d6ec60ba467e2f0362a339c30
Srushti Sawant <98805433+SrushtiSawant15@users.noreply.github.com>
2022-11-25 14:22:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const state = {
-  taskList: [],
+  taskList: [], 
 };
 
-// DOM - document object
+
 const taskContents = document.querySelector(".task__contents");
 const taskModal = document.querySelector(".task__modal__body");
 
